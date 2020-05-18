f6e7139740e28b09625ea24270228045819683f2
DominyRonin <45741480+DominyRonin@users.noreply.github.com>
2020-05-18 16:40:09 +0200

    Add files via upload

---
+++ AdminDoctorControl.php
@@ -0,0 +1,83 @@
+<html>
+
+    <head>
+        <title>Dr Hisham medical center</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300, 400, 700" rel="stylesheet">
+    
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+        <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+        <link rel="stylesheet" href="css/jquery.timepicker.css">
+    
+        <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
+        <link rel="stylesheet" href="fonts/fontawesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <style>
+        
+    
+          
+           .logo
+       {
+        width: 100px;
+        height: 100px; 
+       } 
+          </style>
+    
+    
+        <!-- Theme Style -->
+        <link rel="stylesheet" href="css/style.css">
+      </head>
+
+
+
+<body>
+     
+<?php include ("Admin.php");?>
+<section class="container">
+<div class="row">
+  <div class="column">
+    <div class="card" style="width: 18rem;">
+  <img src="img/1200px-OOjs_UI_icon_add.svg.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Add New Doctor</h5>
+    <p class="card-text">You can add approved doctor from here</p>
+    <a href="AddDoctorView.php" class="btn btn-primary">Add</a>
+  </div>
+</div>
+  </div>
+    <hr>
+  <div class="column">
+    <div class="card" style="width: 18rem;">
+  <img src="img/subtract8-512.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Delete Doctor</h5>
+    <p class="card-text">Delete any Doctor you want </p>
+    <a href="DeleteDoctor.php" class="btn btn-primary">Delete</a>
+  </div>
+</div>
+  </div>
+     <hr>
+  <div class="column">
+    <div class="card" style="width: 18rem;">
+  <img src="img/refresh+reload+update+icon-1320191166843452904.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Update doctor</h5>
+    <p class="card-text">you can update doctor info here</p>
+    <a href="EditDoctor.php" class="btn btn-primary">Update</a>
+  </div>
+</div>
+  </div>
+  <div class="column">
+  
+  </div>
+</div>
+   </section> 
+</body>
+    
+
+
+</html>
