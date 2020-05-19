6072bf27b36c6d7c622ee5bf766486f503e93b3e
DominyRonin <45741480+DominyRonin@users.noreply.github.com>
2020-05-19 03:59:42 +0200

    Add files via upload

--- Registerview.php
+++ Registerview.php
@@ -1,80 +1,39 @@
 <html>
 <head>
-    <head>
-        <title>Dr Hisham medical center</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300, 400, 700" rel="stylesheet">
     
-        <link rel="stylesheet" href="css/bootstrap.css">
-        <link rel="stylesheet" href="css/animate.css">
-        <link rel="stylesheet" href="css/owl.carousel.min.css">
-        <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-        <link rel="stylesheet" href="css/jquery.timepicker.css">
-    
-        <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
-        <link rel="stylesheet" href="fonts/fontawesome/css/font-awesome.min.css">
-        <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-           <style>
-    
-        {
-            align-content: center;
-        }
-    
-    </style>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/all.min.css">
+  <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/signin_style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <style>
+    
         
+    </style>
+    </head>
+<body>
+    <?php
     
-          
-           .logo
-       {
-        width: 100px;
-        height: 100px; 
-       } 
-          </style>
+    ini_set('display_errors',1);
+ini_set('track_errors',1);
+ini_set('display_startup_errors',1);
+ini_set('log_errors',1);
+ini_set('error_log',dirname(__FILE__).'/log.txt');
+error_reporting(-1);
+error_reporting(E_ALL | E_STRICT);
     
-    
-        <!-- Theme Style -->
-        <link rel="stylesheet" href="css/style.css">
-      </head>
-
-<?php include ("Admin.php");
-
-include_once ("AddDoctorModel.php");
-include_once("AddDoctorController.php");
-$model=new AddDoctorModel();
-$controller=new AddDoctorController($model);
-
-
-if (isset($_GET['action']) && !empty($_GET['action'])) {
- $controller->{$_GET['action']}();
-}
-
-
-?>
-
-
-
-
-    <body>
-        
- 
-      <?php
-   
+    include "header.php";
     include_once("connection.php");
-    
    
     
     ?>
-
-    <div class="container">
-     <h2 >Add a doctor</h2>
-    <form action="AddDoctorView.php?action=insertuser" method="post">
+     <div class="container">
+     <h2 >Sign Up</h2>
+    <form action="LoginController.php" method="post">
   <div class="form-group">
     <label for="exampleFormControlInput1">First name </label>
-    <input type="text" name="fname" class="form-control" id="exampleFormControlInput1" required> 
+    <input type="text" name="fname" class="form-control" id="exampleFormControlInput1" required>
   </div>
         <div class="form-group">
     <label for="exampleFormControlInput1">Last name</label>
@@ -86,32 +45,33 @@ if (isset($_GET['action']) && !empty($_GET['action'])) {
   </div>
         <div class="form-group">
     <label for="exampleFormControlInput1">Password</label>
-    <input type="password" name="password" id="psw" class="form-control"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required >
+    <input type="password" name="password" id="psw" class="form-control"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" id="exampleFormControlInput1" required >
   </div>
         <div class="form-group">
     <label for="exampleFormControlInput1">confirm password</label>
-    <input type="password" name="password" id="psw2" class="form-control"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required >
+    <input type="password" name="password" id="psw2" class="form-control"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" id="exampleFormControlInput1" required >
   </div>
+        
         <div class="form-group">
     <label for="exampleFormControlInput1">Phone number</label>
-    <input type="text" name="phone" class="form-control" id="exampleFormControlInput1"  required>
+    <input type="text" name="phone" class="form-control" id="exampleFormControlInput1" required >
   </div>
          <div class="form-group">
                     <label for="birthDate" class="col-sm-3 control-label">Date of Birth</label>
                     <div class="col-sm-9">
-                        <input type="date" name="date" id="birthDate" class="form-control" required>
+                        <input type="date" name="date" id="birthDate" class="form-control" value="2020-05-19"min="1950-01-01" max="2020-05-19" required>
                     </div>
                 </div>
   <div class="form-group">
     <label for="exampleFormControlSelect1">Gender</label>
-    <select class="form-control" name="gender" id="exampleFormControlSelect1" value="2020-05-19"min="1950-01-01" max="2020-05-19" required>
+    <select class="form-control" name="gender" id="exampleFormControlSelect1">
       <option>male</option>
       <option>female</option>
       
     </select>
       
   </div>
-        <button type="submit" name="submit" id="sup" class="btn btn-primary">Sign UP</button>
+        <button type="submit" name="register" id="sup" class="btn btn-primary">Sign UP</button>
     </form>
     
     </div>
@@ -133,7 +93,6 @@ function validatePassword(){
 
 myInput.onchange = validatePassword;
 myInput2.onkeyup = validatePassword;
-
 // When the user clicks on the password field, show the message box
 myInput.onfocus = function() {
   document.getElementById("message").style.display = "block";
@@ -143,6 +102,7 @@ myInput.onfocus = function() {
 myInput.onblur = function() {
   document.getElementById("message").style.display = "none";
 }
+
 
 // When the userstype something inside the password field
 myInput.onkeyup = function() {
@@ -184,13 +144,17 @@ myInput.onkeyup = function() {
     length.classList.remove("valid");
     length.classList.add("invalid");
   }
+    function validatePassword(){
+  if(myInput.value != myInput2.value) {
+    myInput2.setCustomValidity("Passwords Don't Match");
+  } else {
+    myInput2.setCustomValidity('');
+  }
+}
+ 
 }
     </script>
-   
-
     
-</body>
-
-
+    </body>
 
 </html>
